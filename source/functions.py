66227ec45710afcc856372f2d5d06c15febe158d
Alessandro Gianfelici <alessandro_gianfelici@swissre.com>
2022-03-04 10:14:14 +0100

    added precommit, linkedin refactoring

--- source/functions.py
+++ source/functions.py
@@ -1,13 +1,32 @@
 import pandas as pd
 import plotly.graph_objects as go
+import yaml
 
-def plot_candle(df):
-    fig = go.Figure(data=[go.Candlestick(x=pd.to_datetime(df['Date']),
-                                         open=df['Open'],
-                                         high=df['High'],
-                                         low=df['Low'],
-                                         close=df['Close'])])
-    fig.update(layout_yaxis_range = [0,max(df['High'] *1.1)])
+
+def load_yaml(filename: str) -> dict:
+    """
+    Utility function to load a yaml file into a pyhon dict
+    Parameters:
+    - filename: str -> fullpath of the yaml file
+    """
+    assert filename.endswith("yaml") or filename.endswith(
+        "yml"
+    ), "Not a yaml extention!"
+    with open(filename, "r", encoding="utf-8") as handler:
+        return yaml.load(handler, Loader=yaml.FullLoader)
+
+
+def plot_candle(price_data):
+    fig = go.Figure(
+        data=[
+            go.Candlestick(
+                x=pd.to_datetime(price_data["Date"]),
+                open=price_data["Open"],
+                high=price_data["High"],
+                low=price_data["Low"],
+                close=price_data["Close"],
+            )
+        ]
+    )
+    fig.update(layout_yaxis_range=[0, max(price_data["High"] * 1.1)])
     return fig
-
-
--- source/linkedin.py
+++ source/linkedin.py
@@ -1,67 +1,57 @@
+import os
 import re
 from datetime import date
 from time import sleep
 
 import pandas as pd
 from bs4 import BeautifulSoup
+from functions import load_yaml
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-linkedin = {'AZM.MI': 'azimutgroup',
-            'BMED.MI': 'banca-mediolanum',
-            'CRES.MI': 'coima',
-            'ENI.MI': 'eni',
-            'FCT.MI': 'fincantieri',
-            'G.MI': 'generali',
-            'IRE.MI': 'iren-gruppo',
-            'IG.MI': 'italgas',
-            'LDO.MI': 'leonardo_company',
-            'PST.MI': 'poste-italiane',
-            'SRG.MI': 'snam-s-p-a-',
-            'TITR.MI': 'tim',
-            'UCG.MI' : 'unicredit',
-            'TRN.MI': 'terna',
-            'UNI.MI': 'unipol-gruppo',
-            'WBD.MI': 'webuildgroup',
-            'MT.MI': 'maire-tecnimont'}
+root_folder = os.path.dirname(os.path.dirname(os.path.normpath(__file__)))
+linkedin = load_yaml(os.path.join(root_folder, "config", "linkedin.yaml"))
 
-class linkedinScraper:
+
+class LinkedinScraper:
     def __init__(self):
-        self.email = 'gianfo90@gmail.com'
-        self.password = 'Vitriol_01'
+        self.email = "gianfo90@gmail.com"
+        self.password = "Vitriol_01"
         self.driver = webdriver.Chrome()
-    
+
     @staticmethod
     def jobs_url(company):
         return f"https://www.linkedin.com/company/{linkedin[company]}/jobs/"
-    
+
     def connect(self):
-        self.driver.get('https://www.linkedin.com/login')
+        self.driver.get("https://www.linkedin.com/login")
         sleep(3)
-        self.driver.find_element_by_id('username').send_keys(self.email)
-        self.driver.find_element_by_id('password').send_keys(self.password)
-        self.driver.find_element_by_id('password').send_keys(Keys.RETURN)
+        self.driver.find_element_by_id("username").send_keys(self.email)
+        self.driver.find_element_by_id("password").send_keys(self.password)
+        self.driver.find_element_by_id("password").send_keys(Keys.RETURN)
         return self.driver
-    
+
     @staticmethod
     def get_employees_number(soup):
-        s1 = soup.get_text()
-        index = s1.find('"employeeCount":')
-        s2 = s1[index+1:]
-        n_employee = int(re.search(r'"employeeCount":(.*?),"callToAction"', s2).group(1))
+        s_1 = soup.get_text()
+        index = s_1.find('"employeeCount":')
+        s_2 = s_1[index + 1 :]
+        n_employee = int(
+            re.search(r'"employeeCount":(.*?),"callToAction"', s_2).group(1)
+        )
         return n_employee
-    
+
     @staticmethod
     def get_open_jobs(soup):
         s1 = soup.get_text()
         index = s1.find('"employeeCount":')
-        s2 = s1[index+1:]
-        s3_part = (re.search(r'"total":(.*?),"links"', s2).group(0))
+        s2 = s1[index + 1 :]
+        s3_part = re.search(r'"total":(.*?),"links"', s2).group(0)
         s3_index = s1.find(s3_part)
-        s3 = s1[s3_index+1:]
+        s3 = s1[s3_index + 1 :]
         n_opening = int(re.search(r'"total":(.*?),"links"', s3).group(1))
         return n_opening
-    
+
     def scrape(self, company_list):
         results = {}
         driver = self.connect()
@@ -69,31 +59,40 @@ class linkedinScraper:
             print(f"Scraping {company}")
             sleep(5)
             driver.get(self.jobs_url(company))
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            soup = BeautifulSoup(driver.page_source, "html.parser")
             try:
-                results[company] = {'open_jobs' : self.get_open_jobs(soup),
-                                'tot_employees' : self.get_employees_number(soup)}
-                print(f"Scraped {company}: {results[company]['open_jobs']} open jobs over {results[company]['tot_employees']}")
+                results[company] = {
+                    "open_jobs": self.get_open_jobs(soup),
+                    "tot_employees": self.get_employees_number(soup),
+                }
+                print(
+                    f"Scraped {company}: {results[company]['open_jobs']} open jobs over {results[company]['tot_employees']}"
+                )
                 sleep(5)
             except:
                 pass
         return results
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     employees_dict = {}
     today = date.today()
     yyyymm = today.year * 100 + today.month
     filename = "open_positions.csv"
-    old = pd.read_csv(filename).rename(columns={'Unnamed: 0':'company'}).set_index('company')
-    current_month = old.loc[old['YYYYMM'] == yyyymm]
-    to_scrape = set(linkedin.keys())-set(current_month.index)
+    old = (
+        pd.read_csv(filename)
+        .rename(columns={"Unnamed: 0": "company"})
+        .set_index("company")
+    )
+    current_month = old.loc[old["YYYYMM"] == yyyymm]
+    to_scrape = set(linkedin.keys()) - set(current_month.index)
     while len(to_scrape):
-        my_scraper = linkedinScraper()
+        my_scraper = LinkedinScraper()
         employees_dict_tmp = my_scraper.scrape(to_scrape)
         employees_dict = employees_dict | employees_dict_tmp
-        to_scrape = set(to_scrape)-set(employees_dict.keys())
+        to_scrape = set(to_scrape) - set(employees_dict.keys())
 
     results = pd.DataFrame(employees_dict).T
-    results['YYYYMM'] = yyyymm
+    results["YYYYMM"] = yyyymm
     pd.concat([old, results]).to_csv(filename)
